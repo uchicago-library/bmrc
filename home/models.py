918f00a50e89fcad00b1ce618f0cf3fbdc5ac28e
Kathy Zadrozny <kzadrozny@uchicago.edu>
2019-11-27 12:00:48 -0600

    Changed excerpt to TextField and made tabs into spaces

--- home/models.py
+++ home/models.py
@@ -3,11 +3,11 @@ from django.db import models
 from modelcluster.fields import ParentalKey
 
 from wagtail.admin.edit_handlers import (
-	FieldPanel,
-	InlinePanel,
-	MultiFieldPanel,
-	PageChooserPanel,
-	StreamFieldPanel,
+    FieldPanel,
+    InlinePanel,
+    MultiFieldPanel,
+    PageChooserPanel,
+    StreamFieldPanel,
 )
 from wagtail.core.fields import RichTextField, StreamField
 from wagtail.core.models import Page, Orderable
@@ -18,79 +18,79 @@ from news.models import NewsStoryPage
 
 
 class HomePageCarouselImages(Orderable):
-	"""Up to 3 images for the home page carousel."""
+    """Up to 3 images for the home page carousel."""
 
-	page = ParentalKey("home.HomePage", related_name="carousel_images")
-	carousel_image = models.ForeignKey(
-		"wagtailimages.Image",
-		null=True,
-		blank=True,
-		on_delete=models.SET_NULL,
-		related_name="+",
-	)
-	carousel_title = models.CharField(max_length=100, blank=True, null=True)
-	carousel_text = RichTextField(blank=True, null=True)
-	button_link = models.ForeignKey(
-		"wagtailcore.Page",
-		null=True,
-		blank=True,
-		on_delete=models.SET_NULL,
-		help_text='Where you want the button to go',
-		related_name="+",
-	)
-	button_label = models.CharField(max_length=100, null=True, blank=True, help_text='Text that shows up in button')
+    page = ParentalKey("home.HomePage", related_name="carousel_images")
+    carousel_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    carousel_title = models.CharField(max_length=100, blank=True, null=True)
+    carousel_text = RichTextField(blank=True, null=True)
+    button_link = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        help_text='Where you want the button to go',
+        related_name="+",
+    )
+    button_label = models.CharField(max_length=100, null=True, blank=True, help_text='Text that shows up in button')
 
-	panels = [
-		ImageChooserPanel("carousel_image"),
-		FieldPanel("carousel_title"),
-		FieldPanel("carousel_text"),
-		PageChooserPanel("button_link"),
-		FieldPanel("button_label"),
-	]
-	heading="Carousel Images",
+    panels = [
+        ImageChooserPanel("carousel_image"),
+        FieldPanel("carousel_title"),
+        FieldPanel("carousel_text"),
+        PageChooserPanel("button_link"),
+        FieldPanel("button_label"),
+    ]
+    heading="Carousel Images",
 
 
 class HomePage(Page):
-	"""Home page model"""
+    """Home page model"""
 
-	template = "home/home_page.html"
-	max_count = 1
-	parent_page_type = [
-		'wagtailcore.Page'
-	]
+    template = "home/home_page.html"
+    max_count = 1
+    parent_page_type = [
+        'wagtailcore.Page'
+    ]
 
-	body = StreamField(
-		[
-			("richtext", blocks.RichtextBlock()),
-			("page_callout", blocks.PageCallout()),
-			("webfeed", blocks.WebFeedBlock()),
-			("new_row", blocks.NewRow()),
-		],
-		null=True,
-		blank=True,
-	)
+    body = StreamField(
+        [
+            ("richtext", blocks.RichtextBlock()),
+            ("page_callout", blocks.PageCallout()),
+            ("webfeed", blocks.WebFeedBlock()),
+            ("new_row", blocks.NewRow()),
+        ],
+        null=True,
+        blank=True,
+    )
 
-	content_panels = Page.content_panels + [
-		MultiFieldPanel(
-			[InlinePanel("carousel_images", max_num=3, label="Image")],
-			heading="Carousel Images",
-		),
-		StreamFieldPanel("body"),
-	]
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [InlinePanel("carousel_images", max_num=3, label="Image")],
+            heading="Carousel Images",
+        ),
+        StreamFieldPanel("body"),
+    ]
 
-	# News Feed
-	def get_context(self, request):
-		context = super(HomePage, self).get_context(request)
-		context["news_feed"] = self.news_feed()
-		return context
+    # News Feed
+    def get_context(self, request):
+        context = super(HomePage, self).get_context(request)
+        context["news_feed"] = self.news_feed()
+        return context
 
-	def news_feed(self):
-		# Order by most recent date first
-		news_feed = NewsStoryPage.objects.live().public().order_by('-story_date')[:3]
+    def news_feed(self):
+        # Order by most recent date first
+        news_feed = NewsStoryPage.objects.live().public().order_by('-story_date')[:3]
 
-		return news_feed
+        return news_feed
 
-	class Meta:
+    class Meta:
 
-		verbose_name = "Home Page"
-		verbose_name_plural = "Home Pages"
+        verbose_name = "Home Page"
+        verbose_name_plural = "Home Pages"
--- news/models.py
+++ news/models.py
@@ -6,8 +6,8 @@ from django.db import models
 from django.utils import timezone
 
 from wagtail.admin.edit_handlers import (
-	FieldPanel,
-	StreamFieldPanel,
+    FieldPanel,
+    StreamFieldPanel,
 )
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
@@ -17,75 +17,75 @@ from streams import blocks
 
 
 class NewsIndexPage(Page):
-	"""Lists all news story pages."""
+    """Lists all news story pages."""
 
-	# template = "blog/blog_listing_page.html"
-	# ajax_template = "blog/blog_listing_page_ajax.html"
-	max_count = 1
-	subpage_types = ['news.NewsStoryPage']
+    # template = "blog/blog_listing_page.html"
+    # ajax_template = "blog/blog_listing_page_ajax.html"
+    max_count = 1
+    subpage_types = ['news.NewsStoryPage']
 
-	def get_context(self, request, *args, **kwargs):
-		"""Custom items to context."""
+    def get_context(self, request, *args, **kwargs):
+        """Custom items to context."""
 
-		context = super().get_context(request, *args, **kwargs)
-		# Get all posts
-		all_posts = NewsStoryPage.objects.live().public().order_by('-story_date')
+        context = super().get_context(request, *args, **kwargs)
+        # Get all posts
+        all_posts = NewsStoryPage.objects.live().public().order_by('-story_date')
 
-		# Paginate all posts by 9 per page
-		paginator = Paginator(all_posts, 9)
-		# Try to get the ?page=x value
-		page = request.GET.get("page")
-		try:
-			# If the page exists and the ?page=x is an int
-			posts = paginator.page(page)
-		except PageNotAnInteger:
-			# If the ?page=x is not an int; show the first page
-			posts = paginator.page(1)
-		except EmptyPage:
-			# If the ?page=x is out of range (too high most likely)
-			# Then return the last page
-			posts = paginator.page(paginator.num_pages)
+        # Paginate all posts by 9 per page
+        paginator = Paginator(all_posts, 9)
+        # Try to get the ?page=x value
+        page = request.GET.get("page")
+        try:
+            # If the page exists and the ?page=x is an int
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            # If the ?page=x is not an int; show the first page
+            posts = paginator.page(1)
+        except EmptyPage:
+            # If the ?page=x is out of range (too high most likely)
+            # Then return the last page
+            posts = paginator.page(paginator.num_pages)
 
-		context["posts"] = posts
-		return context
+        context["posts"] = posts
+        return context
 
 
 class NewsStoryPage(Page):
-	"""News story page model"""
+    """News story page model"""
 
-	# template = "standard/standard_page.html"
+    # template = "standard/standard_page.html"
 
-	subpage_types = []
-	parent_page_types = ['news.NewsIndexPage']
+    subpage_types = []
+    parent_page_types = ['news.NewsIndexPage']
 
-	lead_image = models.ForeignKey(
-		"wagtailimages.Image",
-		blank=True,
-		null=True,
-		related_name="+",
-		on_delete=models.SET_NULL,
-	)
-	excerpt = models.CharField(max_length=300, blank=True, null=True)
-	body = StreamField(
-		[
-			("richtext", blocks.RichtextBlock()),
-			("page_callout", blocks.PageCallout()),
-			("image_block", blocks.ImageBlock()),
-			("new_row", blocks.NewRow()),
-		],
-		null=True,
-		blank=True,
-	)
-	story_date = models.DateField(default=timezone.now, help_text='Defaults to date page was created. If you plan to publish in the future post, change to publish date here.')
+    lead_image = models.ForeignKey(
+        "wagtailimages.Image",
+        blank=True,
+        null=True,
+        related_name="+",
+        on_delete=models.SET_NULL,
+    )
+    excerpt = models.TextField(max_length=300, blank=True, null=True)
+    body = StreamField(
+        [
+            ("richtext", blocks.RichtextBlock()),
+            ("page_callout", blocks.PageCallout()),
+            ("image_block", blocks.ImageBlock()),
+            ("new_row", blocks.NewRow()),
+        ],
+        null=True,
+        blank=True,
+    )
+    story_date = models.DateField(default=timezone.now, help_text='Defaults to date page was created. If you plan to publish in the future post, change to publish date here.')
 
-	content_panels = Page.content_panels + [
-		ImageChooserPanel("lead_image"),
-		FieldPanel("excerpt"),
-		StreamFieldPanel("body"),
-		FieldPanel('story_date'),
-	]
+    content_panels = Page.content_panels + [
+        ImageChooserPanel("lead_image"),
+        FieldPanel("excerpt"),
+        StreamFieldPanel("body"),
+        FieldPanel('story_date'),
+    ]
 
-	class Meta:
+    class Meta:
 
-		verbose_name = "News Story"
-		verbose_name_plural = "News Stories"
+        verbose_name = "News Story"
+        verbose_name_plural = "News Stories"
