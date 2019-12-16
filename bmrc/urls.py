40fdb157d3ed2b0444640bcdf38c7214a9e5dd12
Kathy Zadrozny <kzadrozny@uchicago.edu>
2019-12-16 11:45:25 -0600

    Added shib items per Brown setup

--- bmrc/urls.py
+++ bmrc/urls.py
@@ -16,6 +16,8 @@ urlpatterns = [
 
     url(r'^search/$', search_views.search, name='search'),
 
+    url(r'^shib/', include('shibboleth.urls', namespace='shibboleth')),
+
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
